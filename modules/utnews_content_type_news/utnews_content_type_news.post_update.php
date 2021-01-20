e81b4068fa6934759d857f410a939ba803eb29dd
Polo Reynaga <lreynaga@austin.utexas.edu>
2021-01-20 11:53:55 -0600

    Add module enabled conditional

--- modules/utnews_content_type_news/utnews_content_type_news.post_update.php
+++ modules/utnews_content_type_news/utnews_content_type_news.post_update.php
@@ -19,19 +19,21 @@ function utnews_content_type_news_post_update_add_social_sharing_block() {
  * Configure XMLSitemap settings.
  */
 function utnews_content_type_news_post_update_configure_xmlsitemap() {
-  if (\Drupal::config('xmlsitemap.settings.node.utnews_news')->get('status') === NULL) {
-    $config = \Drupal::configFactory()->getEditable('xmlsitemap.settings.node.utnews_news');
-    $config_path = drupal_get_path('module', 'utnews_content_type_news') . '/config/install/xmlsitemap.settings.node.utnews_news.yml';
-    if (!empty($config_path)) {
-      $data = Yaml::parse(file_get_contents($config_path));
-      if (is_array($data)) {
-        $config->setData($data)->save(TRUE);
+  if (\Drupal::moduleHandler()->moduleExists('xmlsitemap') !== FALSE) {
+    if (\Drupal::config('xmlsitemap.settings.node.utnews_news')->get('status') === NULL) {
+      $config = \Drupal::configFactory()->getEditable('xmlsitemap.settings.node.utnews_news');
+      $config_path = drupal_get_path('module', 'utnews_content_type_news') . '/config/install/xmlsitemap.settings.node.utnews_news.yml';
+      if (!empty($config_path)) {
+        $data = Yaml::parse(file_get_contents($config_path));
+        if (is_array($data)) {
+          $config->setData($data)->save(TRUE);
+        }
       }
     }
-  }
-  else {
-    $message = dt('XML Sitemap configuration object "xmlsitemap.settings.node.utnews_news" found. No action taken.');
-    \Drupal::messenger()->addMessage($message);
-    \Drupal::logger('utexas')->notice($message);
+    else {
+      $message = dt('XML Sitemap configuration object "xmlsitemap.settings.node.utnews_news" found. No action taken.');
+      \Drupal::messenger()->addMessage($message);
+      \Drupal::logger('utexas')->notice($message);
+    }
   }
 }
