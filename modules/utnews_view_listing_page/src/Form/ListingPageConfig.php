03744ae1b534275e15aa1e15122d163625a52474
Mark Fullmer <jfullmer@austin.utexas.edu>
2021-07-29 10:21:57 -0500

    33 - Allow content editors to configure display on news overview page (#133)
    * Add ability to toggle display states for news listing page
    
    * Finalize schema
    
    * Capture configuration changes to listing page View
    
    * Remove unnecessary fallback values
    
    * Add configuration form cache tag to Views field handler instead of invalidating cached view display when submitting form changes
    
    * Remove unnecessary check for View object
    
    * Remove unused Cache class
    
    * Relocate node_view manipulation to parent module

--- modules/utnews_view_listing_page/src/Form/ListingPageConfig.php
+++ modules/utnews_view_listing_page/src/Form/ListingPageConfig.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\utnews_view_listing_page\Form;
 
-use Drupal\Core\Cache\Cache;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\utnews\Form\BaseConfigurationForm;
 
@@ -15,14 +14,30 @@ class ListingPageConfig extends BaseConfigurationForm {
    * The actual form elements.
    */
   public function alterForm(&$form, FormStateInterface $form_state, $form_id) {
-    $default_title = $this->configFactory->get('utnews_view_listing_page.config')->get('page_title');
+    $config = $this->configFactory->get('utnews_view_listing_page.config');
     $form['page_title'] = [
       '#title' => $this->t('News listing page title'),
       '#type' => 'textfield',
-      '#default_value' => $default_title ?? 'News',
+      '#default_value' => $config->get('page_title'),
       '#description' => $this->t('Displayed on <a href=":link">:link</a>.', [
         ':link' => \Drupal::request()->getSchemeAndHttpHost() . '/news',
       ]),
+    ];
+
+    $form['display_thumbnail'] = [
+      '#title' => $this->t('Display thumbnail images on news listing page.'),
+      '#type' => 'checkbox',
+      '#default_value' => $config->get('display_thumbnail'),
+    ];
+    $form['display_date'] = [
+      '#title' => $this->t('Display dates on news listing page.'),
+      '#type' => 'checkbox',
+      '#default_value' => $config->get('display_date'),
+    ];
+    $form['display_summary'] = [
+      '#title' => $this->t('Display article summaries on news listing page.'),
+      '#type' => 'checkbox',
+      '#default_value' => $config->get('display_summary'),
     ];
 
     $form['#submit'][] = [$this, 'submitListingConfig'];
@@ -32,14 +47,18 @@ class ListingPageConfig extends BaseConfigurationForm {
    * Extended submit callback.
    */
   public function submitListingConfig(&$form, FormStateInterface $form_state) {
-    $page_title = $form_state->getValue('page_title');
-    if ($page_title) {
-      $config = $this->configFactory->getEditable('utnews_view_listing_page.config');
-      $config->set('page_title', $page_title);
-      $config->save();
-      // Ensure that this change invalidates the view cache.
-      Cache::invalidateTags(['config:views.view.utnews_listing_page']);
+    $form_settings = [
+      'page_title',
+      'display_thumbnail',
+      'display_date',
+      'display_summary',
+    ];
+    $config = $this->configFactory->getEditable('utnews_view_listing_page.config');
+    foreach ($form_settings as $setting) {
+      $value = $form_state->getValue($setting);
+      $config->set($setting, $value);
     }
+    $config->save();
   }
 
 }
