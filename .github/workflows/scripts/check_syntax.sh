3af906a0fe07baf5674ebf33c9ca152dc7d2888e
jmf3658 <jfullmer@austin.utexas.edu>
2024-03-11 09:24:05 -0500

    Get more depth in git fetch for syntax checker

--- .github/workflows/scripts/check_syntax.sh
+++ .github/workflows/scripts/check_syntax.sh
@@ -9,9 +9,11 @@ TOOLING=".github/workflows/scripts/syntax_checker.json"
 #git clone https://github.com/utdk/$REPO.git
 #cd $REPO
 
+# GitHub Actions performs a 'shallow' fetch, so we get more history with --depth
+# See https://stackoverflow.com/a/59570673/6046296
 git checkout -f
-git fetch && git checkout develop
-git fetch && git checkout $BRANCH
+git fetch --depth=100 && git checkout develop
+git fetch --depth=100 && git checkout $BRANCH
 
 $COMPOSER_CMD validate --no-check-all
 # If there are composer validation issues, this will send an exit code of 1.
